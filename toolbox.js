8fdf98adc21bbf91799993e995aec53719be2ccc
smnwlsn <wilson182@gmail.com>
2019-11-06 15:30:13 +0100

    Update toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -1,5 +1,5 @@
 function init() {
-    Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vRPj5ANy-5hloo0kQoA2wkVXf1AJ9zOLy_QxvRJkJWrsnCO19T321Nx5yXwziQRlIbXhJWJUjCucqMD/pubhtml',
+    Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/2PACX-1vRPj5ANy-5hloo0kQoA2wkVXf1AJ9zOLy_QxvRJkJWrsnCO19T321Nx5yXwziQRlIbXhJWJUjCucqMD/pubhtml',
     callback: function(data, tabletop) {
     console.log(data)
     },
